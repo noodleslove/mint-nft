9c6a3d341d11c59558d818986d7532c85eafd048
Eddie Ho <boho@ucsd.edu>
2023-10-27 00:11:37 -0700

    Complete lesson

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -11,7 +11,7 @@ async function main() {
   // mint one to yourself!
   const signer0 = await ethers.provider.getSigner(0);
   // update the IPFS CID to be your metadata CID
-  await nft.safeMint(await signer0.getAddress(), "ipfs://QmbCwoJB7g9b86BfEahsjzyzzNx3WEu7Jc1opXWgeTXFgb");
+  await nft.safeMint(await signer0.getAddress(), "ipfs://QmSgvgwxZGaBLqkGyWemEDqikCqU52XxsYLKtdy3vGZ8uq");
 
   console.log("NFT Minted!");
 }
